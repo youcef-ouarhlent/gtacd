aebeb73fff8fd8b8e9faf7ea76e7489c7837091e
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2025-07-31 14:12:28 +0200

    modification test

--- app/check_app.sh
+++ app/check_app.sh
@@ -5,8 +5,8 @@ echo "🔍 Début de la vérification de l'application..."
 if [ -f "app/index.html" ]; then
     echo "✅ index.html trouvé."
     echo "✅ Vérification réussie."
-    exit 0
+    return 0
 else
     echo "❌ Fichier index.html manquant."
-    exit 1
+    return 1
 fi
